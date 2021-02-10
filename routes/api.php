7d4078e572ab34c0a6b8d4dde817a760ac2ca83c
axcherednikov <agel63@mail.ru>
2021-02-10 14:26:52 +0300

    deleted

--- routes/api.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Http\Controllers\Api\Auth\AuthController;
-use Illuminate\Support\Facades\Route;
-
-Route::prefix('auth')->group(function () {
-    Route::post('register', [AuthController::class, 'register'])->name('api.auth.register');
-    Route::post('login', [AuthController::class, 'token'])->name('api.auth.login');
-});
